6840fa806b108ef3877670e3d9286d257a8b8564
yifeng002 <493788939lizhaogai@163.com>
2017-03-06 15:06:51 +0800

    1.0.0

--- index.js
+++ index.js
@@ -10,10 +10,20 @@
  * Do not edit the class manually.
  *
  */
-'use strict';
 
 const Requestor = require('./lib/_requestor');
 const Account = require('./lib/Account');
+const Tenant = require('./lib/Tenant');
+const Record = require('./lib/Record');
+const Balance = require('./lib/Balance');
+const Withdraw = require('./lib/Withdraw');
+const Invitation = require('./lib/Invitation');
+const SocialGroup = require('./lib/SocialGroup');
+const Income = require('./lib/Income');
+const Taobaoke = require('./lib/Taobaoke');
+const Settings = require('./lib/Settings');
+const SocialGroupStatus = require('./lib/SocialGroupStatus');
+const CommissionRecord = require('./lib/CommissionRecord');
 /**
  * Money Mommy API Explorer.
  *
@@ -40,6 +50,17 @@ module.exports = function(baseUrl, opts) {
   };
 
   client.account = new Account(requestor);
+  client.tenant = new Tenant(requestor);
+  client.record = new Record(requestor);
+  client.balance = new Balance(requestor);
+  client.withdraw = new Withdraw(requestor);
+  client.invitation = new Invitation(requestor);
+  client.socialGroup = new SocialGroup(requestor);
+  client.income = new Income(requestor);
+  client.taobaoke = new Taobaoke(requestor);
+  client.settings = new Settings(requestor);
+  client.socialGroupStatus = new SocialGroupStatus(requestor);
+  client.commissionRecord = new CommissionRecord(requestor);
 
   return client;
 };
